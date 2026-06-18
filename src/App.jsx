41007a6f26ed60a8ef8a23ca91eafa9339b2f990
RAVALSAVANS <166410589+RAVALSAVANS@users.noreply.github.com>
2026-06-18 12:48:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import SolutionsPage from "./pages/SolutionsPage";
 import ContactPage from "./pages/ContactPage";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
+import NotFound from "./pages/NotFound";
 
 // Scroll to top on route change
 function ScrollToTop() {
@@ -45,6 +46,7 @@ function App() {
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/privacy" element={<PrivacyPolicy />} />
             <Route path="/terms" element={<TermsOfService />} />
+             <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
           <ScrollTop />
